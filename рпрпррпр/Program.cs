3fb85c7669404fed60ffe508c0c74f901133d5b8
Liromanz <kiss_wiwa_2003@mail.ru>
2023-09-14 16:36:36 +0300

    jhgjhgjhg

---
+++ рпрпррпр/Program.cs
@@ -0,0 +1,19 @@
+﻿string txt;
+do
+{
+    Console.WriteLine("Какой цвет вы любите?");
+    Console.WriteLine("1. Черный");
+    Console.WriteLine("2. Белый");
+    Console.WriteLine("3. Серобуромалиновый");
+    Console.WriteLine("4. Прозрачный");
+    Console.WriteLine("5. никакой(((");
+    txt = Console.ReadLine();
+    if (txt == "1")
+    {
+        Console.WriteLine("а почему не белый");
+    }
+    else if (txt == "2")
+    {
+        Console.WriteLine("а почему не черный");
+    }
+} while (txt != "5");
